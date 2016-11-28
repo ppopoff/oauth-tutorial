ccf9b26d50424d481a5e07a157f01a2adbf2a5ba
Paul Popoff <oc14@ya.ru>
2016-11-28 14:50:44 +0300

    initial commit

---
+++ src/main/java/com/doingfp/oauth/Foo.java
@@ -0,0 +1,42 @@
+package com.doingfp.oauth;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+public final class Foo {
+
+    @JsonProperty("id")
+    private long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    public Foo(final long id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public final Foo setId(final long newId) {
+        return new Foo(newId, name);
+    }
+
+    public final Foo setName(final String newName) {
+        return new Foo(id, newName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Foo foo = (Foo) o;
+        return id == foo.id &&
+                Objects.equals(name, foo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
