6446e66f9c6a16fbe329aa58e47fa693eb9fede0
Paul Popoff <oc14@ya.ru>
2016-11-29 04:57:41 +0300

    Renamed

--- resource/src/main/java/com/doingfp/oauth/ResourceServerApplication.java
+++ resource/src/main/java/com/doingfp/oauth/ResourceServerApplication.java
@@ -1,14 +1,12 @@
 package com.doingfp.oauth;
 
-//import com.doingfp.oauth.service.ServerSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@Import({ ServerSecurityConfig.class, OAuth2ResourceServerConfig.class })
-public class Application extends SpringBootServletInitializer {
+public class ResourceServerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ResourceServerApplication.class, args);
     }
 }
