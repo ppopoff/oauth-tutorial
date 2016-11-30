d574723136176871723757eac044f0f93c11566d
Paul Popoff <oc14@ya.ru>
2016-11-30 10:39:18 +0300

    prePost removed

--- resource/src/main/java/com/doingfp/oauth/MethodSecurityConfig.java
+++ resource/src/main/java/com/doingfp/oauth/MethodSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecur
 
 @Configuration
 @EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
     @Override
