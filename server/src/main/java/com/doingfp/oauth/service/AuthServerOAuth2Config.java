143207113c2802aee77248f2f3be9f9d1e631b91
Paul Popoff <oc14@ya.ru>
2016-12-01 14:47:12 +0300

    Using JWT

--- server/src/main/java/com/doingfp/oauth/service/AuthServerOAuth2Config.java
+++ server/src/main/java/com/doingfp/oauth/service/AuthServerOAuth2Config.java
@@ -7,21 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import java.util.Arrays;
 
@@ -40,25 +36,28 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
     @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
 
-    @Value("classpath:schema.sql")
-    private Resource schemaScript;
+//    @Value("classpath:schema.sql")
+//    private Resource schemaScript;
 
     @Autowired
     private Environment env;
 
-    @Override
-    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer.allowFormAuthenticationForClients();
-    }
+//    @Override
+//    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+//        oauthServer.allowFormAuthenticationForClients();
+//    }
 
+    /**
+     * @param clients a number of clients to configure
+     */
     @Override
     public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("test")
+                .withClient("test") // client id
                 .secret("test")
                 .autoApprove(true)
                 .authorizedGrantTypes("code", "authorization_code", "refresh_token", "password")
-                .scopes("read");
+                .scopes("read", "write");
     }
 
 
@@ -68,43 +67,44 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
         TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
         tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
 
-        endpoints.tokenStore(tokenStore())
+        endpoints.tokenStore(jwtTokenStore())
                  .tokenEnhancer(tokenEnhancerChain)
                  .authenticationManager(authenticationManager);
     }
 
     @Bean
     public TokenEnhancer tokenEnhancer() {
-        return new CustomTokenEnhancer();
+        return new LocationTokenEnhancer();
     }
 
     @Bean
-    public TokenStore tokenStore() {
+    public TokenStore jwtTokenStore() {
         return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
     }
 
 //    @Bean
 //    public JwtAccessTokenConverter accessTokenConverter() {
 //        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setSigningKey("123");
+//        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+//            new ClassPathResource("mytest.jks"), "mypass".toCharArray()
+//        );
+//        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mytest"));
 //        return converter;
 //    }
 
     @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        KeyStoreKeyFactory keyStoreKeyFactory =
-          new KeyStoreKeyFactory(new ClassPathResource("mytest.jks"), "mypass".toCharArray());
-        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mytest"));
-        return converter;
-    }
-
-    @Bean
     @Primary
     public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(jwtTokenStore());
+        //defaultTokenServices.setSupportRefreshToken(true);
         return defaultTokenServices;
     }
 
