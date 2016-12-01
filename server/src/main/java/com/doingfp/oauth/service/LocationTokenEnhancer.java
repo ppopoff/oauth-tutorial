63f6a1f6d51f914e85e8d06c6636a001bc8e68a7
Paul Popoff <oc14@ya.ru>
2016-12-01 14:41:06 +0300

    Renamed + Formatting

--- server/src/main/java/com/doingfp/oauth/service/LocationTokenEnhancer.java
+++ server/src/main/java/com/doingfp/oauth/service/LocationTokenEnhancer.java
@@ -8,11 +8,12 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CustomTokenEnhancer implements TokenEnhancer {
+public class LocationTokenEnhancer implements TokenEnhancer {
     @Override
-    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+    public OAuth2AccessToken enhance(final OAuth2AccessToken accessToken,
+                                     final OAuth2Authentication authentication) {
         final Map<String, Object> additionalInfo = new HashMap<>();
-        additionalInfo.put("loc", "Voronezh");
+        additionalInfo.put("location", "Voronezh");
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
